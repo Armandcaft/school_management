c4c0462582a62c7634da59edd21faf02254dcf80
Armandcaft <71290581+Armandcaft@users.noreply.github.com>
2023-03-19 08:52:43 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Students\LessonController;
+use App\Http\Controllers\Teachers\CourseController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+//ROutes redirection corresponding to roles.
+Route::group(['middleware' => 'auth'], function() {
+    Route::group(['middleware' => 'role:student', 'prefix' => 'student', 'as' => 'student.'], function() {
+        Route::resource('lessons', LessonController::class);
+    });
+    Route::group(['middleware' => 'role:teacher', 'prefix' => 'teacher', 'as' => 'teacher.'], function() {
+        Route::resource('courses', CourseController::class);
+    });
+    Route::group(['middleware' => 'role:admin', 'prefix' => 'admin', 'as' => 'admin.'], function() {
+        Route::resource('users', UserController::class);
+    });
+});
+
